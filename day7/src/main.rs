7c18dde93f0b70cd897ea1deb5d946f7bff85655
Andreas Christianson <achristianson@pessimistic-it.com>
2023-12-07 01:21:49 -0600

    playing a little code golf

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut hands = vec![];
     for line in lines {
         let parts: Vec<&str> = line.split(' ').collect();
-        let cards_in_hand=parts[0]
+        let cards_in_hand = parts[0]
             .replace("T", "a")
             .replace("J", "0")
             // .replace("J", "b") // part1
@@ -16,36 +16,23 @@ fn main() {
         let hand = Hand {
             rank: find_rank(cards_in_hand.clone()),
             cards: cards_in_hand,
-            bid: parts[1].parse::<u32>().unwrap(),
+            bid: parts[1].parse::<usize>().unwrap(),
         };
         hands.push(hand);
     }
     hands.sort();
-    let mut sum = 0;
-    let mut pos = 0;
-    for hand in hands {
-        pos = pos + 1;
-        sum = pos * hand.bid + sum;
-    }
+    let sum = hands.iter().enumerate().fold(
+        0,
+        |sum, (index, hand)| sum + (index + 1) * hand.bid
+    );
     println!("{}", sum);
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Rank {
-    HighCard,
-    OnePair,
-    TowPair,
-    Three,
-    Full,
-    Four,
-    Five,
-}
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Hand {
-    rank: Rank,
+    rank: usize,
     cards: String,
-    bid: u32,
+    bid: usize,
 }
 
 
@@ -59,52 +46,29 @@ fn read_lines(filename: &str) -> Vec<String> {
     result
 }
 
-fn find_rank(hand: String) -> Rank {
-    let char_count_map = find_dup_chars(hand.clone());
-    let mut largest = 0;
-    let mut second_largest = 0;
-    let mut jokers = 0;
-    for (c, count) in char_count_map {
-        if c == '0' {
-            jokers = count;
-            continue;
-        }
-        match count {
-            count if count > largest => {
-                second_largest = largest;
-                largest = count;
-            }
-            count if count > second_largest => {
-                second_largest = count
-            }
-            _ => ()
-        }
-    }
-    largest=largest+jokers;
+fn find_rank(hand: String) -> usize {
+    let mut char_count_map = find_char_count_map(hand.clone());
+    let jokers = char_count_map.remove(&'0');
 
-    if largest == 5 {
-        return Rank::Five;
-    }
-    if largest == 4 {
-        return Rank::Four;
-    }
+    let mut counts: Vec<usize> = char_count_map.into_iter()
+        .map(|(_, count)| count)
+        .collect();
+    counts.sort();
+
+    let largest = counts.pop().unwrap_or(0) + jokers.unwrap_or(0);
+    let second_largest = counts.pop().unwrap_or(0);
+
     if largest == 3 && second_largest == 2 {
-        return Rank::Full;
-    }
-    if largest == 3 {
-        return Rank::Three;
+        return 35;
     }
     if largest == 2 && second_largest == 2 {
-        return Rank::TowPair;
+        return 25;
     }
-    if largest == 2 {
-        return Rank::OnePair;
-    }
-    return Rank::HighCard;
+    return largest * 10;
 }
 
-fn find_dup_chars(string: String) -> HashMap<char, isize> {
-    let mut chars: HashMap<char, isize> = HashMap::new();
+fn find_char_count_map(string: String) -> HashMap<char, usize> {
+    let mut chars: HashMap<char, usize> = HashMap::new();
 
     for c in string.chars() {
         let count = chars.entry(c).or_insert(0);
