393a79d0470d4c7d06547199f487aad26cfc314d
Andreas Christianson <achristianson@pessimistic-it.com>
2023-12-12 02:00:40 -0600

    cleanup

--- day12/index.ts
+++ day12/index.ts
@@ -1,12 +1,10 @@
 import * as fs from 'fs';
 import {Memoize, clear} from "typescript-memoize";
 
-// export function hello(who: string = world): string {
-//     return `Hello ${who}! `;
-// }
-
-
-const allContents = fs.readFileSync('input.txt', 'utf-8');
+const parameters = fs.readFileSync('input.txt', 'utf-8')
+    .split("\n")
+    .filter(line => line.length > 0)
+    .map(line => line.split(" "))
 
 enum Spring {
     ok = ".",
@@ -22,11 +20,10 @@ const toSpring = (springString: string): Spring => {
 
 
 class HotSpringRow {
-    checkSum: number[];
-    private springs: Spring[];
+    private readonly checkSum: number[];
+    private readonly springs: Spring[];
 
     constructor(dataString: string, checksumString: string) {
-
         this.checkSum = checksumString
             .split(",")
             .filter(str => str.length > 0)
@@ -36,22 +33,12 @@ class HotSpringRow {
             .map(springString => toSpring(springString))
     }
 
-    @Memoize({
-            hashFunction: (springPos: number, checkSumPos: number) => {
-                return springPos + ";" + checkSumPos ;
-            },
-            tags: ["row"]
-        }
-    )
+    @Memoize((...args) => args.join(";"))
     walk(springPos = 0, checkSumPos = 0): number {
-        // console.log("walking at ", springPos, checkSumPos)
         if (springPos == this.springs.length) {
-            // console.log("walk: at end of row ")
             if (checkSumPos == this.checkSum.length) {
-                // console.log(row, " Accepted while walking!")
                 return 1
             }
-            // console.log(row, " under checksum count")
             return 0
         }
         switch (this.springs[springPos]) {
@@ -64,83 +51,50 @@ class HotSpringRow {
         }
     }
 
-    @Memoize({
-            hashFunction: (springPos: number, checkSumPos: number, brokenCount: number) => {
-                return springPos + ";" + checkSumPos + ";"+ brokenCount;
-            },
-            tags: ["row"]
-        }
-    )
+    @Memoize((...args) => args.join(";"))
     private collectBroken(springPos: number, checkSumPos: number, brokenCount = 0): number {
-        // console.log("collecting at ", springPos, checkSumPos, brokenCount)
-        if (springPos == this.springs.length) {
-            // console.log("collect: at end of row")
+        if (springPos === this.springs.length) {
             if (this.checkSum[checkSumPos] == brokenCount && checkSumPos === this.checkSum.length - 1) {
-                // console.log(row, " Accepted while collecting!")
                 return 1
             }
-            // console.log(row, " incomplete broken threshold at end of row")
             return 0
         }
         if (checkSumPos === this.checkSum.length) {
-            // console.log(row, " exceeded checksum count")
             return 0
         }
         if (brokenCount > this.checkSum[checkSumPos]) {
-            // console.log(row, " exceeded broken threshold")
             return 0
         }
 
         switch (this.springs[springPos]) {
             case Spring.ok:
-                if (this.checkSum[checkSumPos] == brokenCount) {
-                    // console.log("complete broken threshold at working spring")
+                if (this.checkSum[checkSumPos] === brokenCount) {
                     return this.walk(springPos, checkSumPos + 1)
                 }
-                // console.log(row, " incomplete broken threshold at working spring")
                 return 0
             case Spring.broken:
-                // console.log("continuing to collect")
                 return this.collectBroken(springPos + 1, checkSumPos, brokenCount + 1)
             case Spring.unknown:
-                if (this.checkSum[checkSumPos] == brokenCount) {
-                    // console.log(" complete broken threshold at unknown spring")
+                if (this.checkSum[checkSumPos] === brokenCount) {
                     return this.walk(springPos + 1, checkSumPos + 1)
                 }
-                // console.log("incomplete broken threshold at unknown spring")
                 return this.collectBroken(springPos + 1, checkSumPos, brokenCount + 1)
         }
     }
 }
 
-const part1 = allContents.split("\n")
-    .filter(line => line.length > 0)
-    .map(line => {
-        const [dataString, checksumString] = line
-            .split(" ")
-        const spring = new HotSpringRow(dataString, checksumString)
-        console.log(spring)
-        clear(["row"])
-        const count = spring.walk()
-        console.log(count)
-        return count
-    })
+const part1 = parameters
+    .map(([dataString, checksumString]) => new HotSpringRow(dataString, checksumString))
+    .map(spring => spring.walk())
+    .reduceRight((r, l) => r + l);
 console.log(part1)
-console.log(part1.reduceRight((left, right) => left + right))
 
-const part2 = allContents.split("\n")
-    .filter(line => line.length > 0)
-    .map(line => {
-        const [dataString, checksumString] = line
-            .split(" ")
+const part2 = parameters
+    .map(([dataString, checksumString]) => {
         const bigSprings = Array<string>(5).fill(dataString).join("?")
         const bigChecksums = (checksumString + ",").repeat(5)
-        const spring = new HotSpringRow(bigSprings, bigChecksums)
-        console.log(spring)
-        clear(["row"])
-        const count = spring.walk()
-        console.log(count)
-        return count
+        return new HotSpringRow(bigSprings, bigChecksums)
     })
+    .map(spring => spring.walk())
+    .reduceRight((r, l) => r + l);
 console.log(part2)
-console.log(part2.reduceRight((left, right) => left + right))
