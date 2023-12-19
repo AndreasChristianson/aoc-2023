61c953331695b74036a59213612f2ad5befa597a
Andreas Christianson <achristianson@pessimistic-it.com>
2023-12-18 22:51:56 -0600

    bootstrapping

---
+++ day19/src/main/java/com/pessimistic/Aoc.java
@@ -0,0 +1,14 @@
+package com.pessimistic;
+
+/**
+ * Hello world!
+ *
+ */
+public class Aoc
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
